8c024ec24e5b0a4bbe5635cab0bbe108115d7a45
runoneall <runoobsteve@126.com>
2024-08-11 23:33:07 +0800

    update files

--- Plugins/plugin_Sqlite/connect_db.py
+++ Plugins/plugin_Sqlite/connect_db.py
@@ -1,6 +1,13 @@
 import sqlite3
 
+def dict_factory(cursor, row):  
+    d = {}  
+    for idx, col in enumerate(cursor.description):  
+        d[col[0]] = row[idx]  
+    return d
+
 def return_cursor(db_file_path:str):
     database = sqlite3.connect(db_file_path)
+    database.row_factory = dict_factory
     cursor = database.cursor()
     return database, cursor
--- Plugins/plugin_Sqlite/execute.py
+++ Plugins/plugin_Sqlite/execute.py
@@ -2,6 +2,11 @@ import sqlite3
 
 database = None
 
+def Execute(cursor, sql:str):
+    cursor.execute(sql)
+    database.commit()
+    return cursor.fetchall()
+
 def create_table(cursor, table_info:dict):
     sql = str()
     table_name = table_info['name']
@@ -13,11 +18,23 @@ def create_table(cursor, table_info:dict):
         item_type = item[item_key]
         sql += f'{item_key} {item_type}, '
     sql = sql[:-2] + ')'
-    cursor.execute(sql)
-    database.commit()
+    Execute(cursor, sql)
 
 def drop_table(cursor, table_name:str):
     table_name = table_name.replace(' ', '_')
-    cursor.execute(f'DROP TABLE IF EXISTS {table_name};')
-    database.commit()
+    Execute(cursor, f'DROP TABLE IF EXISTS {table_name};')
 
+def insert(cursor, insert_info:str, ):
+    table_name = insert_info['name']
+    table_name = table_name.replace(' ', '_')
+    insert_items = insert_info['item']
+    keys = str()
+    values = str()
+    for item in insert_items:
+        item_key = list(item.keys())[0]
+        keys += f'{item_key},'
+        item_value = item[item_key]
+        values += f'{item_value},'
+    keys = keys[:-1]
+    values = values[:-1]
+    Execute(cursor, f'INSERT INTO {table_name} ({keys}) VALUES ({values});')
