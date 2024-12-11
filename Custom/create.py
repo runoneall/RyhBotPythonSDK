5fcd301358efa4b020897b50fd265a4c9da292e3
runoneall <runoobsteve@hotmail.com>
2024-12-11 23:29:06 +0800

    update custom tool (server)

--- Custom/create.py
+++ Custom/create.py
@@ -119,7 +119,68 @@ if "message" in config:
     print("Message Created")
 
 if "server" in config:
-    print("Creating server plugin...")
+    print("Creating Server...")
+    file_content = str()
+    file_content_var_area = str()
+    file_content_class_area = str()
+    file_content_router_area = str()
+    file_content += server_template.header
+    file_content_class_area += server_template.class_header
+    file_content_router_area += server_template.router_header
+    for need in config['server']:
+        if need == "normal":
+            file_content_var_area += server_template.normal_var
+            file_content_class_area += server_template.normal_class
+            file_content_router_area += server_template.normal_router
+            continue
+        if need == "command":
+            file_content_var_area += server_template.command_var
+            file_content_class_area += server_template.command_class
+            file_content_router_area += server_template.command_router
+            continue
+        if need == "button":
+            file_content_var_area += server_template.button_var
+            file_content_class_area += server_template.button_class
+            file_content_router_area += server_template.button_router
+            continue
+        if need == "all":
+            file_content_var_area += server_template.all_var
+            file_content_class_area += server_template.all_class
+            file_content_router_area += server_template.all_router
+            continue
+        if need == "bot-add":
+            file_content_var_area += server_template.bot_add_var
+            file_content_class_area += server_template.bot_add_class
+            file_content_router_area += server_template.bot_add_router
+            continue
+        if need == "bot-delete":
+            file_content_var_area += server_template.bot_delete_var
+            file_content_class_area += server_template.bot_delete_class
+            file_content_router_area += server_template.bot_delete_router
+            continue
+        if need == "bot-settings":
+            file_content_var_area += server_template.bot_settings_var
+            file_content_class_area += server_template.bot_settings_class
+            file_content_router_area += server_template.bot_settings_router
+            continue
+        if need == "user-join":
+            file_content_var_area += server_template.user_join_var
+            file_content_class_area += server_template.user_join_class
+            file_content_router_area += server_template.user_join_router
+            continue
+        if need == "user-leave":
+            file_content_var_area += server_template.user_leave_var
+            file_content_class_area += server_template.user_leave_class
+            file_content_router_area += server_template.user_leave_router
+            continue
+    file_content_router_area += server_template.router_footer
+    file_content += file_content_var_area
+    file_content += file_content_class_area
+    file_content += file_content_router_area
+    file_content += server_template.server_start
+    with open(f"{root_dir}/Server.py", "w", encoding="utf-8") as server_file:
+        server_file.write(file_content)
+    print("Server Created")
 
 if config['allow-plugin'] == True:
     print("Creating plugin directory...")
---
+++ Custom/server_template.py
@@ -0,0 +1,203 @@
+header = '''
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+'''
+
+class_header = '''
+
+class Message:
+'''
+
+router_header = '''
+@app.route('/', methods=['POST'])
+def RecvMsg():
+    data = request.json
+'''
+
+router_footer = '''
+    with open("RecvMsg.json", "w", encoding="utf-8") as RecvMsg:
+        json.dump(data, RecvMsg, ensure_ascii=False, indent=4)
+    return jsonify(data)
+'''
+
+server_start = '''
+def Start(host: str, port: int, debug: bool = False):
+    app.run(
+        host=host,
+        port=port,
+        debug=debug
+    )
+'''
+
+normal_var = '''
+NormalFuncList = []'''
+normal_class = '''
+    class Normal:
+        def __init__(self, func) -> None:
+            global NormalFuncList
+            self.func = func
+            NormalFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+normal_router = '''
+    if data['header']['eventType'] == "message.receive.normal":
+        for func in NormalFuncList:
+            func(data=data['event'])
+'''
+
+command_var = '''
+CommandFuncList = []'''
+command_class = '''
+    class Command:
+        def __init__(self, func) -> None:
+            global CommandFuncList
+            self.func = func
+            CommandFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+command_router = '''
+    if data['header']['eventType'] == "message.receive.instruction":
+        for func in CommandFuncList:
+            func(data=data['event'])
+'''
+
+button_var = '''
+ButtonClickFuncList = []'''
+button_class = '''
+    class ButtonClick:
+        def __init__(self, func) -> None:
+            global ButtonClickFuncList
+            self.func = func
+            ButtonClickFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+button_router = '''
+    if data['header']['eventType'] ==  "button.report.inline":
+        for func in ButtonClickFuncList:
+            func(data=data['event'])
+'''
+
+all_var = '''
+AllTypeFuncList = []'''
+all_class = '''
+    class AllType:
+        def __init__(self, func) -> None:
+            global AllTypeFuncList
+            self.func = func
+            AllTypeFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+all_router = '''
+    if AllTypeFuncList != []:
+        for func in AllTypeFuncList:
+            func(data=data['event'])
+'''
+
+bot_add_var = '''
+BotFollowedFuncList = []'''
+bot_add_class = '''
+    class BotFollowed:
+        def __init__(self, func) -> None:
+            global BotFollowedFuncList
+            self.func = func
+            BotFollowedFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+bot_add_router = '''
+    if data['header']['eventType'] == "bot.followed":
+        for func in BotFollowedFuncList:
+            func(data=data['event'])
+'''
+
+bot_delete_var = '''
+BotUnFollowedFuncList = []'''
+bot_delete_class = '''
+    class BotUnFollowed:
+        def __init__(self, func) -> None:
+            global BotUnFollowedFuncList
+            self.func = func
+            BotUnFollowedFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+bot_delete_router = '''
+    if data['header']['eventType'] == "bot.unfollowed":
+        for func in BotUnFollowedFuncList:
+            func(data=data['event'])
+'''
+
+bot_settings_var = '''
+BotSettingsFuncList = []'''
+bot_settings_class = '''
+    class BotSettings:
+        def __init__(self, func) -> None:
+            global BotSettingsFuncList
+            self.func = func
+            BotSettingsFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+bot_settings_router = '''
+    if data['header']['eventType'] == "bot.setting":
+        for func in BotSettingsFuncList:
+            func(data=data['event'])
+'''
+
+user_join_var = '''
+GroupJoinFuncList = []'''
+user_join_class = '''
+    class GroupJoin:
+        def __init__(self, func) -> None:
+            global GroupJoinFuncList
+            self.func = func
+            GroupJoinFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+user_join_router = '''
+    if data['header']['eventType'] == "group.join":
+        for func in GroupJoinFuncList:
+            func(data=data['event'])
+'''
+
+user_leave_var = '''
+GroupLeaveFuncList = []'''
+user_leave_class = '''
+    class GroupLeave:
+        def __init__(self, func) -> None:
+            global GroupLeaveFuncList
+            self.func = func
+            GroupLeaveFuncList.append(func)
+
+        def __call__(self, *args, **kwds):
+            rv = self.func(*args, **kwds)
+            return rv
+'''
+user_leave_router = '''
+    if data['header']['eventType'] == "group.leave":
+        for func in GroupLeaveFuncList:
+            func(data=data['event'])
+'''
