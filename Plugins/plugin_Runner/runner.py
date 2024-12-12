1ce644152e8d94598603a9e6f7c41923d5e272d7
runoneall <runoobsteve@hotmail.com>
2024-12-12 12:07:51 +0800

    update

--- Plugins/plugin_Runner/runner.py
+++ Plugins/plugin_Runner/runner.py
@@ -4,15 +4,15 @@ proc: Process = None
 
 def run(target, *args, **kwargs):
     global proc
-    print("--- Code Running In Runner ---")
+    print("[#] Code Running In Runner")
     proc = Process(target=target, args=args, kwargs=kwargs)
     proc.start()
 
 def stop():
     global proc
-    print("--- Stopping Runner ---")
+    print("[#] Stopping Runner")
     if proc is not None and proc.is_alive():
         proc.terminate()
         proc.join()
         proc = None
-    print("--- Runner Stopped ---")
+    print("[#] Runner Stopped")
