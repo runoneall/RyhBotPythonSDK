cf5e7a8eb2b6acf7d2ee546d314fd83ac7b243e3
runoneall <runoobsteve@hotmail.com>
2024-12-12 15:45:24 +0800

    update

--- Plugins/plugin_Runner/install.py
+++
@@ -1,9 +0,0 @@
-funcname = 'Runner'
-attrs = {
-    'Runner': 'runner',
-    'UpdateServer': 'update_server'
-}
-author = 'runoneall'
-version = '1.0'
-introduction = '''Add Runner Support For RyhBotPythonSDK
-Can Update Code From Remote.'''
